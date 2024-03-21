52a612898f5a76f71d8aabecf68125a8beae613d
tara rajput <tara378581@gmail.com>
2024-03-22 01:30:43 +0500

    push my simple_calculator on github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# tara-CLI-simple_calculator" 
---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\nWelcome to\`Tara_rajput\`CLI simple_calculator\n");
+const answer = await inquirer.prompt([
+  {message: "Enter your first number", type: "number", name: "firstnumber"},
+
+  {message: "Enter your second number", type: "number", name: "secondnumber"},
+
+  {
+    message: "select one of the operators to perform the operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division","Exponention"],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+}else if(answer.operator === "Exponention"){
+  console.log(answer.firstnumber ** answer.secondnumber);
+}else{
+  console.log("please select valid operator");
+}
+
+
+  
